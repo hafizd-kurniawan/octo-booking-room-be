614cb5839b8dbc26e2500c285045d62179301ec0
Aan Nur Wahidi <aannurwahidi117@gmail.com>
2026-03-15 12:16:55 +0700

    feat: add cors and swagger documentation

---
+++ src/main/java/com/octo/booking_room/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.octo.booking_room.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:5137", "https://octo-room-fe.vercel.app/")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true);
+    }
+}
---
+++ src/main/java/com/octo/booking_room/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.octo.booking_room.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+@RestController
+public class HomeController {
+
+    @GetMapping({"/", "/docs"})
+    public RedirectView home() {
+        return new RedirectView("/swagger-ui/index.html");
+    }
+}
